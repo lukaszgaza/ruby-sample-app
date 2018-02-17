2ef77043db6ec373a4d69851f36274d548ca2f22
Lukasz Gaza <g.lukasz@gmail.com>
2018-02-17 23:21:53 +0100

    Added validation of name and email: emptyness, length, format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAILE_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAILE_REGEX},
+            uniqueness: {case_sensitive: false}
 end
